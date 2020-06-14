1843ad0e574b3d92e3f392adb2a9e48678101b99
YS <yue.shang@jd.com>
2020-06-15 04:13:25 +0800

    runnable dnn with tfexample input

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
 class Config:
+    visible_gpus = "0,1,2,3"
     idx_text = 1
     idx_label = 6
-    vocab_file="/home/search/Data/yue.shang/bert/glue_data/SST-2/new_data.parts/words.txt"
-    embedding_size = 50
-    dropout = 0.5
-    hidden_units = [512,128]
+    vocab_file="./dict/words.txt"
+    embedding_size = 64
+    epoches = 3
+    dropout = 0
+    hidden_units = [1024, 512,128, 64]
+    train_batch_size = 128
+    regularizer_scale = 0.01
+    learning_rate = 0.01
+    data_dir = "./tfrecords"
+    input_files = "part-*[1-9].tfrecord"
+    eval_files = "part-*0.tfrecord"
+    model_path = "models/dnn"
+    label_thres = 0.5
--- run_train.py
+++ run_train.py
@@ -1,46 +1,26 @@
 import tensorflow as tf
-from tensorflow.keras.datasets import imdb
-from tensorflow.keras.preprocessing import sequence
 import numpy as np
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
 import csv
 from tensorboard import summary as summary_lib
 import collections
-from tokenizer import Tokenizer
-from config import Config
 import braceexpand
 from natsort import natsorted
 import glob
 
+from config import Config
+# from tokenizer import Tokenizer
 
 flags = tf.flags
 FLAGS = flags.FLAGS
-
-flags.DEFINE_string("data_dir", "/home/search/Data/yue.shang/data/amazon_movie_reviews/shuf/", 
-    "the directory of input files")
-flags.DEFINE_string("input_files", "part-*[1-9].tfrecord", 
-    "The glob pattern fopr input files")
-flags.DEFINE_string("eval_files", "part-*0.tfrecord", 
-    "The glob pattern for input files")
-
-flags.DEFINE_string("model_path", "./models/dnn_test", "model output path")
 flags.DEFINE_bool("train_and_eval", True, "Whether to run training.")
 flags.DEFINE_bool(
     "do_predict", False,
     "Whether to run the model in inference mode on the test set.")
-flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
-flags.DEFINE_integer("num_examples", 650000, "Total num of train examples")
-flags.DEFINE_float("num_train_epochs", 3.0,
-                   "Total number of training epochs to perform.")
-flags.DEFINE_bool("gen_data_only", True,
-                  "Whether to generate train/eval/test data only.")
-
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+os.environ['CUDA_VISIBLE_DEVICES'] = Config.visible_gpus
 tf.logging.set_verbosity(tf.logging.INFO)
 #########################
-
-
 
 def file_based_input_fn_builder(input_files_pattern, is_training):
     """Creates an `input_fn` closure to be passed to TPUEstimator."""
@@ -63,17 +43,22 @@ def file_based_input_fn_builder(input_files_pattern, is_training):
         print("input files:")
         print(filenames)
         d = tf.data.TFRecordDataset(filenames)
+        d = d.repeat(Config.epoches)
+        d = d.shuffle(buffer_size=2000)
         d = d.map(_decode_record)
-        d = d.batch(FLAGS.train_batch_size)
+        d = d.batch(Config.train_batch_size)
         return d
 
     return input_fn
 
 def dnn_model_fn(features, labels, mode, params):
     feature_columns = params['feature_columns']
-    # print(feature_columns)
+    # <_EmbeddingColumn, len() = 8>
+
     net = tf.feature_column.input_layer(features, feature_columns)
-    regularizer = tf.contrib.layers.l2_regularizer(0.05)
+    # <tf.Tensor 'input_layer/concat:0' shape=(?, 50) dtype=float32>
+
+    regularizer = tf.contrib.layers.l2_regularizer(Config.regularizer_scale)
     for units in Config.hidden_units:
         net = tf.layers.dense(
             net, units=units, activation=tf.nn.relu, 
@@ -87,6 +72,7 @@ def dnn_model_fn(features, labels, mode, params):
             kernel_regularizer=regularizer)
     
     probs = tf.nn.sigmoid(logits)
+    # <tf.Tensor 'Sigmoid:0' shape=(?, 1) dtype=float32>
     if mode == tf.estimator.ModeKeys.PREDICT:
         predictions = {}
         predictions['logits'] = logits
@@ -94,24 +80,30 @@ def dnn_model_fn(features, labels, mode, params):
         return tf.estimator.EstimatorSpec(mode, predictions=predictions)
     
     # compute loss
-    logits = logits[:, 0]
+    labels = tf.expand_dims(labels, 1) # <tf.Tensor 'ExpandDims:0' shape=(?, 1) dtype=float32>
     loss = tf.losses.sigmoid_cross_entropy(labels, logits)
 
     # compute metrics
-    binary_labels = tf.to_int32(labels)
+    positive_labels = tf.greater(labels, Config.label_thres)
+    binary_labels = tf.to_int32(positive_labels)
     auc = tf.metrics.auc(labels=binary_labels, predictions=probs, name='auc')
     acc = tf.metrics.accuracy(labels=binary_labels, predictions=probs)
+    tf.Print(acc, [acc], first_n=10)
     metrics = {'auc': auc, 'acc':acc}
-
     tf.summary.scalar('loss', loss)
 
     # evaluation mode
     if mode == tf.estimator.ModeKeys.EVAL:
-        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
+        summary_hook = tf.train.SummarySaverHook(
+            save_steps=200, output_dir=Config.model_path, 
+            summary_op=tf.summary.merge_all())
+        eval_hooks = [summary_hook]
+        return tf.estimator.EstimatorSpec(mode, loss=loss, evaluation_hooks=eval_hooks,
+            eval_metric_ops=metrics)
 
     # Create training op.
     assert mode == tf.estimator.ModeKeys.TRAIN
-    optimizer = tf.train.AdagradOptimizer(learning_rate=0.05)
+    optimizer = tf.train.AdagradOptimizer(learning_rate=Config.learning_rate)
     train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
     logging_hook = tf.train.LoggingTensorHook({"loss" : loss, "accuracy" : acc[1]}, 
         every_n_iter=50)
@@ -123,19 +115,19 @@ def dnn_model_fn(features, labels, mode, params):
 if __name__ == "__main__":
     run_config = tf.estimator.RunConfig(
         save_checkpoints_secs=30)
-    train_files_pattern = os.path.join(FLAGS.data_dir, FLAGS.input_files)
-    eval_files_pattern = os.path.join(FLAGS.data_dir, FLAGS.eval_files)
+    train_files_pattern = os.path.join(Config.data_dir, Config.input_files)
+    eval_files_pattern = os.path.join(Config.data_dir, Config.eval_files)
     column = tf.feature_column.categorical_column_with_vocabulary_file(
         key='text', vocabulary_file=Config.vocab_file, num_oov_buckets=0, dtype=tf.string)
     word_embedding_column = tf.feature_column.embedding_column(
-        column, dimension=Config.embedding_size)
+        column, dimension=Config.embedding_size, combiner="sqrtn")
     
     predictor = tf.estimator.Estimator(
         model_fn=dnn_model_fn,
         params={
             "feature_columns": word_embedding_column
         },
-        model_dir=FLAGS.model_path,
+        model_dir=Config.model_path,
         config=run_config)
     
     # if FLAGS.train_and_eval:
@@ -150,12 +142,12 @@ if __name__ == "__main__":
         input_files_pattern=eval_files_pattern,
         is_training=False)
 
-    hooks = [tf.train.ProfilerHook(output_dir=FLAGS.model_path, save_secs=15)]
+    # hooks = [tf.train.ProfilerHook(output_dir=Config.model_path, save_secs=15)]
     train_spec = tf.estimator.TrainSpec(
         input_fn=train_input_fn,
         hooks=[])
     
-    hook = tf.train.ProfilerHook(output_dir=FLAGS.model_path, save_secs=20)
+    # hook = tf.train.ProfilerHook(output_dir=Config.model_path, save_steps=200)
     eval_spec = tf.estimator.EvalSpec(
         input_fn=eval_input_fn)
 
